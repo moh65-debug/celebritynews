8dd352c42e4e290769131e7a14a530f5c947dbe3
GitHub Action <action@github.com>
2026-04-01 20:30:51 +0000

    Update trending news [automated]

--- src/data.ts
+++ src/data.ts
@@ -10,4 +10,115 @@ export interface Article {
     url?: string;
 }
 
-export const articles: Article[] = [];
+export const articles: Article[] = [
+    {
+        "id": "jmltk6z1f",
+        "title": "GoHealth Prioritizes Consumer Fit, Renewal Economics and Cash Discipline While Continuing Leadership in Special Needs Plans; Reports Full Year 2025 Results",
+        "excerpt": "Disciplined posture reflects consumer-first enrollment, an intentional Medicare Advantage pullback driven by market realities, continued special needs plans (“SNP”) leadership, targeted AI investment, and a focus on cash, back-book durability, and consolidati…",
+        "content": "<p>Disciplined posture reflects consumer-first enrollment, an intentional Medicare Advantage pullback driven by market realities, continued special needs plans (“SNP”) leadership, targeted AI investment, and a focus on cash, back-book durability, and consolidati…</p>",
+        "celebrities": [],
+        "category": "Tech",
+        "image_url": "https://ml.globenewswire.com/Resource/Download/cd15364d-1173-4d6a-b32e-8c76d0767842",
+        "date": "Mar 31, 2026",
+        "url": "https://www.globenewswire.com/news-release/2026/03/31/3266065/0/en/GoHealth-Prioritizes-Consumer-Fit-Renewal-Economics-and-Cash-Discipline-While-Continuing-Leadership-in-Special-Needs-Plans-Reports-Full-Year-2025-Results.html"
+    },
+    {
+        "id": "5v3zq4sep",
+        "title": "Report: Serie A club to open talks over move for Tottenham Hotspur star",
+        "excerpt": "Fiorentina Ready to Reignite Interest in Spurs Defender Radu DrăgușinFresh developments from Sports Boom suggest that Tottenham Hotspur defender Radu Drăgușin could be edging closer to a return to...",
+        "content": "<p>Fiorentina Ready to Reignite Interest in Spurs Defender Radu DrăgușinFresh developments from Sports Boom suggest that Tottenham Hotspur defender Radu Drăgușin could be edging closer to a return to...</p>",
+        "celebrities": [],
+        "category": "Sports",
+        "image_url": "https://s.yimg.com/ny/api/res/1.2/uJX0qISKYpo0EkHSkGkRlg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04Njk7Y2Y9d2VicA--/https://media.zenfs.com/en/epl_index_articles_253/a9a497c701d9708bd71322d473a0403c",
+        "date": "Mar 31, 2026",
+        "url": "https://eplindex.com/144743/report-serie-a-club-to-open-talks-over-move-for-tottenham-hotspur-star.html"
+    },
+    {
+        "id": "o8ztaj3al",
+        "title": "California Tightens AI Contract Rules as Fight With Trump Admin Grows",
+        "excerpt": "Governor Gavin Newsom orders stronger safeguards for AI companies seeking California contracts, escalating tensions with the Trump administration over national AI regulation.",
+        "content": "<p>Governor Gavin Newsom orders stronger safeguards for AI companies seeking California contracts, escalating tensions with the Trump administration over national AI regulation.</p>",
+        "celebrities": [],
+        "category": "Tech",
+        "image_url": "https://cdn.decrypt.co/resize/1024/height/512/wp-content/uploads/2025/09/gavin-newsom-decrypt-style-01-gID_7.jpg",
+        "date": "Mar 31, 2026",
+        "url": "https://decrypt.co/362968/california-tightens-ai-contract-rules-fight-trump-admin"
+    },
+    {
+        "id": "gfhi6i1os",
+        "title": "Citizen Science Month 2026 is about more than just stargazing",
+        "excerpt": "This year’s celebration highlights how Americans can support federal missions not only through NASA research, but also by helping preserve the nation’s historical record.",
+        "content": "<p>This year’s celebration highlights how Americans can support federal missions not only through NASA research, but also by helping preserve the nation’s historical record.</p>",
+        "celebrities": [],
+        "category": "Science",
+        "image_url": "https://cdn.nextgov.com/media/img/cd/2026/03/31/GettyImages_2241962139/open-graph.jpg",
+        "date": "Mar 31, 2026",
+        "url": "https://www.nextgov.com/emerging-tech/2026/03/citizen-science-month-2026-about-more-just-stargazing/412530/"
+    },
+    {
+        "id": "2vz354awv",
+        "title": "Nvidia invests $2B in Marvell as part of new interconnect partnership",
+        "excerpt": "Nvidia Corp. today disclosed that it has invested $2 billion in Marvell Technology Inc., a publicly traded semiconductor designer. The cash infusion is part of a new partnership that will span several parts of the chip market. Santa Clara, California-based Ma…",
+        "content": "<p>Nvidia Corp. today disclosed that it has invested $2 billion in Marvell Technology Inc., a publicly traded semiconductor designer. The cash infusion is part of a new partnership that will span several parts of the chip market. Santa Clara, California-based Ma…</p>",
+        "celebrities": [],
+        "category": "Tech",
+        "image_url": "https://d15shllkswkct0.cloudfront.net/wp-content/blogs.dir/1/files/2026/03/Marvell.jpg",
+        "date": "Mar 31, 2026",
+        "url": "https://siliconangle.com/2026/03/31/nvidia-invests-2b-marvell-part-new-interconnect-partnership/"
+    },
+    {
+        "id": "3tb47czel",
+        "title": "Edible Garden Reports 2025 Results — Accelerates Planned Expansion into Higher-Margin Ready-to-Drink (RTD) and Shelf-Stable CPG Platform",
+        "excerpt": "Targets Large, Underserved RTD Market Opportunity with Scalable Midwest Platform and Tetra Pak Integration   Leverages Established Infrastructure, National...",
+        "content": "<p>Targets Large, Underserved RTD Market Opportunity with Scalable Midwest Platform and Tetra Pak Integration   Leverages Established Infrastructure, National...</p>",
+        "celebrities": [],
+        "category": "Sports",
+        "image_url": "https://ml.globenewswire.com/Resource/Download/d358b3e1-f835-4bf2-b91a-05743c591e68",
+        "date": "Mar 31, 2026",
+        "url": "https://www.globenewswire.com/news-release/2026/03/31/3266056/0/en/Edible-Garden-Reports-2025-Results-Accelerates-Planned-Expansion-into-Higher-Margin-Ready-to-Drink-RTD-and-Shelf-Stable-CPG-Platform.html"
+    },
+    {
+        "id": "vgljbahiq",
+        "title": "Tomás Palacios named director of the Institute for Soldier Nanotechnologies",
+        "excerpt": "Tomás Palacios has been named director of the MIT Institute for Soldier Nanotechnologies, which advances fundamental science and engineering to develop next-generation materials and devices for soldier protection, survivability, and sensing capabilities.",
+        "content": "<p>Tomás Palacios has been named director of the MIT Institute for Soldier Nanotechnologies, which advances fundamental science and engineering to develop next-generation materials and devices for soldier protection, survivability, and sensing capabilities.</p>",
+        "celebrities": [],
+        "category": "Science",
+        "image_url": "https://news.mit.edu/sites/default/files/images/202603/Tomas-Palacios-MIT-A1.jpg",
+        "date": "Mar 31, 2026",
+        "url": "https://news.mit.edu/2026/tomas-palacios-named-director-institute-soldier-nanotechnologies-0331"
+    },
+    {
+        "id": "2ld8g1ymc",
+        "title": "The Boys S05: Firecracker Spins A-Train Story; Ryan to Boarding School",
+        "excerpt": "With only days to go until the premiere of the fifth and final season of Showrunner Eric Kripke hits our screens, we're getting a chance to jump back into the show's universe for a look at just how bad the propaganda campaign has become. Think FOX \"News,\" but…",
+        "content": "<p>With only days to go until the premiere of the fifth and final season of Showrunner Eric Kripke hits our screens, we're getting a chance to jump back into the show's universe for a look at just how bad the propaganda campaign has become. Think FOX \"News,\" but…</p>",
+        "celebrities": [],
+        "category": "Breaking",
+        "image_url": "https://bleedingcool.com/wp-content/uploads/2026/03/MixCollage-31-Mar-2026-02-16-PM-4754-2000x1125.jpg",
+        "date": "Mar 31, 2026",
+        "url": "https://bleedingcool.com/tv/the-boys-s05-firecracker-spins-a-train-story-ryan-to-boarding-school/"
+    },
+    {
+        "id": "n05eezxhq",
+        "title": "Celtic Fans Told That They Have Seen The Last Of 24-Year-Old: Will The Side Abandon A Transfer?",
+        "excerpt": "Celtic’s Scottish Premiership title charge hit a massive roadblock this week. Reports confirm that Julian Araujo will likely miss the rest of the season. The 24-year-old defender joined the Glasgow ...",
+        "content": "<p>Celtic’s Scottish Premiership title charge hit a massive roadblock this week. Reports confirm that Julian Araujo will likely miss the rest of the season. The 24-year-old defender joined the Glasgow ...</p>",
+        "celebrities": [],
+        "category": "Sports",
+        "image_url": "https://s.yimg.com/ny/api/res/1.2/0X7YIiErxDRhyEF7P5zkpw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD05MjQ7Y2Y9d2VicA--/https://media.zenfs.com/en/the_4th_official_articles_868/812eb4318faa57f388f47f0e055e1c98",
+        "date": "Mar 31, 2026",
+        "url": "https://the4thofficial.net/2026/03/celtic-fans-told-that-they-have-seen-the-last-of-24-year-old-will-the-side-abandon-a-transfer/"
+    },
+    {
+        "id": "oq32vxgzp",
+        "title": "AVAX One Reports Fourth Quarter and Full Year 2025 Financial and Operating Results",
+        "excerpt": "Total AVAX Holdings of Approximately 13,936,000 as of March 31, 2026  Completed 3.3 Million Share Repurchases Under $40 Million Share Repurchase Program  ...",
+        "content": "<p>Total AVAX Holdings of Approximately 13,936,000 as of March 31, 2026  Completed 3.3 Million Share Repurchases Under $40 Million Share Repurchase Program  ...</p>",
+        "celebrities": [],
+        "category": "Business",
+        "image_url": "https://ml.globenewswire.com/Resource/Download/9708f625-ebde-4a0c-ab4a-58385ceb754f",
+        "date": "Mar 31, 2026",
+        "url": "https://www.globenewswire.com/news-release/2026/03/31/3266017/0/en/AVAX-One-Reports-Fourth-Quarter-and-Full-Year-2025-Financial-and-Operating-Results.html"
+    }
+];
